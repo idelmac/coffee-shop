051a20cfd698b9b3888d955f7a013f545a6e9f52
Idelfonso Macingarrela <idelfonso.macinga@outlook.com>
2022-09-24 20:45:45 +0200

    24/09/2022

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,14 +1,17 @@
 import os
-from urllib import response
 from flask import Flask, request, jsonify, abort
-from sqlalchemy import exc
 import json
 from flask_cors import CORS
 from marshmallow import ValidationError
 import sys
 
-from .database.models import db_drop_and_create_all, setup_db, Drink, DrinkSchema, RecipeSchema
-from .auth.auth import AuthError, check_permissions, requires_auth
+from .database.models import (db_drop_and_create_all,
+                              setup_db,
+                              Drink,
+                              DrinkSchema)
+from .auth.auth import (AuthError,
+                        check_permissions,
+                        requires_auth)
 
 app = Flask(__name__)
 setup_db(app)
@@ -18,19 +21,8 @@ AUTH0_DOMAIN = 'idelmac.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffee-shop'
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this funciton will add one
-'''
 db_drop_and_create_all()
 
-@app.route('/headers')
-@requires_auth('post:drinks')
-def headers(jwt):
-    print(jwt)
-    return 'not implemented'
 
 def format_drinks_short(drinks):
     formated_drinks = []
@@ -38,71 +30,50 @@ def format_drinks_short(drinks):
         formated_drinks.append(drink.short())
     return formated_drinks
 
+
 def format_drinks_long(drinks):
     formated_drinks = []
     for drink in drinks:
         formated_drinks.append(drink.long())
     return formated_drinks
-    
+
+
 # ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks')
 def get_drinks():
     drinks = Drink.query.order_by(Drink.id).all()
     formated_drinks = []
-    
+
     if len(drinks) == 0:
         abort(404)
-            
+
     formated_drinks = format_drinks_short(drinks)
-    
+
     return jsonify({
         "success": True,
         "status": 200,
         "drinks": formated_drinks
     })
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
 def get_drinks_detail(payload):
     drinks = Drink.query.order_by(Drink.id).all()
     formated_drinks = []
-    
+
     if len(drinks) == 0:
         abort(404)
-        
+
     formated_drinks = format_drinks_long(drinks)
-    
+
     return jsonify({
         "success": True,
         "status": 200,
         "drinks": formated_drinks
     })
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route("/drinks", methods=["POST"])
 @requires_auth('post:drinks')
 def create_drink(payload):
@@ -111,6 +82,7 @@ def create_drink(payload):
         schema = DrinkSchema()
         result = schema.load(body)
     except ValidationError as err:
+        print(err.messages)
         return jsonify(err.messages), 400
 
     title = body.get("title", None)
@@ -127,7 +99,10 @@ def create_drink(payload):
         drink = Drink(title=title, recipe=json.dumps(recipe))
         drink.insert()
 
-        new_drink = Drink.query.filter(Drink.title==title).order_by(Drink.id).all()
+        new_drink = Drink.query.filter(
+            Drink.title == title
+            ).order_by(Drink.id).all()
+
         formated_drink = format_drinks_long(new_drink)
 
         return jsonify({
@@ -139,17 +114,7 @@ def create_drink(payload):
         print(sys.exc_info())
         abort(422)
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route("/drinks/<int:drink_id>", methods=["PATCH"])
 @requires_auth('patch:drinks')
 def update_drink(payload, drink_id):
@@ -169,13 +134,16 @@ def update_drink(payload, drink_id):
 
     title = body.get("title", None)
     recipe = body.get("recipe", None)
-    
+
     try:
         drink.title = title
         drink.recipe = json.dumps(recipe)
         drink.update()
 
-        updated_drink = Drink.query.filter(Drink.id==drink_id).all()
+        updated_drink = Drink.query.filter(
+            Drink.id == drink_id
+            ).all()
+
         formated_drink = format_drinks_long(updated_drink)
 
         return jsonify({
@@ -186,17 +154,8 @@ def update_drink(payload, drink_id):
     except Exception:
         print(sys.exc_info())
         abort(422)
-    
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
+
+
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_question(payload, drink_id):
@@ -218,17 +177,16 @@ def delete_question(payload, drink_id):
         print(sys.exc_info())
         abort(422)
 
-# Error Handling
-'''
-Example error handling for unprocessable entity
-'''
 
+# Error Handling
 @app.errorhandler(400)
 def bad_request(error):
-    return (jsonify({"success": False,
-                        "error": 400,
-                        "message": "bad request"}),
+    return (jsonify({
+                    "success": False,
+                    "error": 400,
+                    "message": "bad request"}),
             400)
+
 
 @app.errorhandler(404)
 def not_found(error, message="resource not found"):
@@ -238,6 +196,7 @@ def not_found(error, message="resource not found"):
                     "message": message}),
             404)
 
+
 @app.errorhandler(409)
 def resource_conflict(error, message="resources conflict"):
     return (jsonify({
@@ -245,6 +204,7 @@ def resource_conflict(error, message="resources conflict"):
                     "error": 409,
                     "message": message}),
             409)
+
 
 @app.errorhandler(422)
 def unprocessable(error):
@@ -255,30 +215,8 @@ def unprocessable(error):
     }), 422
 
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
 @app.errorhandler(AuthError)
-def auth_error(error):
-    response = jsonify(error.error)
-    response.status_code = error.status_code
-    
-    return response
+def auth_error(auth_error):
+    exception = jsonify(auth_error.error)
+    exception.status_code = auth_error.status_code
+    return exception
--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -7,14 +7,11 @@ from flask_migrate import Migrate
 
 database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = "sqlite:///{}".format(os.path.join(
+                                                project_dir,
+                                                database_filename))
 
 db = SQLAlchemy()
-
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 
 
 def setup_db(app):
@@ -26,14 +23,6 @@ def setup_db(app):
     migrate = Migrate(app, db)
 
 
-'''
-db_drop_and_create_all()
-    drops the database tables and starts fresh
-    can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
-'''
-
-
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
@@ -42,62 +31,52 @@ def db_drop_and_create_all():
         title='water',
         recipe='[{"name": "water", "color": "blue", "parts": 1}]'
     )
-
-
     drink.insert()
+
 # ROUTES
 
-'''
-Drink
-a persistent drink entity, extends the base SQLAlchemy Model
-'''
 
 class RecipeSchema(Schema):
-    color = fields.String(required=True,
-                            allow_none=False,
-                            validate=validate.Length(min=1))
-    name = fields.String(required=True,
-                             allow_none=False,
-                             validate=validate.Length(min=1))
-    parts = fields.Integer(required=True,
-                             allow_none=False,
-                             validate=validate.Range(min=1))
+    color = fields.String(
+                        required=True,
+                        allow_none=False,
+                        validate=validate.Length(min=1))
+    name = fields.String(
+                        required=True,
+                        allow_none=False,
+                        validate=validate.Length(min=1))
+    parts = fields.Integer(
+                        required=True,
+                        allow_none=False,
+                        validate=validate.Range(min=1))
 
 
 class DrinkSchema(Schema):
-    title = fields.String(required=True,
-                             allow_none=False,
-                             validate=validate.Length(min=1))
-    recipe = fields.Nested(RecipeSchema, required=True)
-    #recipe = fields.List(fields.Nested(RecipeSchema))
+    id = fields.Integer(required=False,
+                        allow_none=True)
+    title = fields.String(
+                            required=True,
+                            allow_none=False,
+                            validate=validate.Length(min=1))
+    recipe = fields.List(fields.Nested(RecipeSchema))
+
 
 class Drink(db.Model):
-    # Autoincrementing, unique primary key
-    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True, autoincrement=True)
-    # String Title
+    id = Column(Integer().with_variant(Integer, "sqlite"),
+                primary_key=True,
+                autoincrement=True)
     title = Column(String(80), unique=True)
-    # the ingredients blob - this stores a lazy json blob
-    # the required datatype is [{'color': string, 'name':string, 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
-    '''
-    short()
-        short form representation of the Drink model
-    '''
-
     def short(self):
-        #print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        short_recipe = [{'color': r['color'], 'parts': r['parts']}
+                        for r in
+                        json.loads(self.recipe)]
         return {
             'id': self.id,
             'title': self.title,
             'recipe': short_recipe
         }
-
-    '''
-    long()
-        long form representation of the Drink model
-    '''
 
     def long(self):
         return {
@@ -106,42 +85,13 @@ class Drink(db.Model):
             'recipe': json.loads(self.recipe)
         }
 
-    '''
-    insert()
-        inserts a new model into a database
-        the model must have a unique name
-        the model must have a unique id or null id
-        EXAMPLE
-            drink = Drink(title=req_title, recipe=req_recipe)
-            drink.insert()
-    '''
-
     def insert(self):
         db.session.add(self)
         db.session.commit()
 
-    '''
-    delete()
-        deletes a new model into a database
-        the model must exist in the database
-        EXAMPLE
-            drink = Drink(title=req_title, recipe=req_recipe)
-            drink.delete()
-    '''
-
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-
-    '''
-    update()
-        updates a new model into a database
-        the model must exist in the database
-        EXAMPLE
-            drink = Drink.query.filter(Drink.id == id).one_or_none()
-            drink.title = 'Black Coffee'
-            drink.update()
-    '''
 
     def update(self):
         db.session.commit()
